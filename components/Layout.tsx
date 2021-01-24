e8827efd24af4c05af0b1a6829fd457daff5274f
Chandan Mandal <19841184+inckmo@users.noreply.github.com>
2021-01-24 14:22:28 +0530

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar"
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col bg-indigo-900">
       <Navbar />
       <div className="flex-auto sm:mt-8 px-4">
         {children}
