8eb2030f68136bc64c534311a69275c321ac29d8
Chandan Mandal <19841184+inckmo@users.noreply.github.com>
2021-01-24 14:12:43 +0530

    Update constant.ts

--- shared/constant.ts
+++ shared/constant.ts
@@ -1,7 +1,7 @@
-export const FULL_NAME = 'Surarerk Boonjit';
-export const POSITION = 'Software Engineer';
-export const GITHUB_USERNAME = 'sujamess';
-export const FACEBOOK_USERNAME = 'sujamess';
-export const TWITTER_USERNAME = 'syujames';
-export const INSTAGRAM_USERNAME = 's6k.j';
-export const LINKEDIN_USERNAME = 'surarerk-boonjit';
+export const FULL_NAME = 'CHANDAN';
+export const POSITION = 'Software Developer';
+export const GITHUB_USERNAME = 'inckmo';
+export const FACEBOOK_USERNAME = '';
+export const TWITTER_USERNAME = '';
+export const INSTAGRAM_USERNAME = '';
+export const LINKEDIN_USERNAME = '';
